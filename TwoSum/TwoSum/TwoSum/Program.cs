80acc6a72e7f12a268a8badaaa6f31205c86bfe7
HongViet <ninhhongviet2@gmail.com>
2025-08-16 17:57:31 +0700

    .-.

--- TwoSum/TwoSum/TwoSum/Program.cs
+++ TwoSum/TwoSum/TwoSum/Program.cs
@@ -4,7 +4,39 @@
     {
         static void Main(string[] args)
         {
+            int[] nums = { 2, 7, 11, 15 };
+            int target = 9;
+            int[] result = TwoSum(nums, target);
+            foreach (int x in result)
+            {
+                Console.Write(x + " ");
+            }
             Console.WriteLine("Hello, World!");
+        }
+
+        static int[] TwoSum(int[] nums, int target)
+        {
+            if (nums == null || nums.Length < 2)
+            {
+                return new int[] { };
+            }
+            int left = 0;
+            int right = nums.Length - 1;
+            while (right >= left)
+            {
+                if (nums[left] + nums[right] == target)
+                {
+                    return new int[] { left, right };
+                }
+
+                if (nums[left] + nums[right] > target)
+                {
+                    right--;
+                }
+                else
+                    left++;     
+            }
+            return new int[] { };
         }
     }
 }
