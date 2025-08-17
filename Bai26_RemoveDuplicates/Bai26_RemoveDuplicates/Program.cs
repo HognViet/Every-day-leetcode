6e5e1aeb9d298c919439882b945a138270a16202
HongViet <ninhhongviet2@gmail.com>
2025-08-17 23:03:46 +0700

    Code

---
+++ Bai26_RemoveDuplicates/Bai26_RemoveDuplicates/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace Bai26_RemoveDuplicates
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] a = { 0, 0, 1, 1, 2, 2, 3, 3, 4, 3 };
+            
+            int k = RemoveDuplicates(a);
+            Console.WriteLine(k); ;
+            Console.WriteLine("Hello, World!");
+        }
+     static int RemoveDuplicates(int[] nums)
+        {
+            int sau = 0;
+            int trc = 1;
+            while (trc <= nums.Length - 1)
+            {
+                if (nums[sau] != nums[trc])
+                {
+                    sau++;
+                    nums[sau] = nums[trc];
+                }
+                else trc++;
+            }
+            return sau + 1;
+        }
+    }
+}   
