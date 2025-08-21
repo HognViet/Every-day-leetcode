d4b3ab9687bcfcd0bbda24e93f89ca8e57d3b364
HongViet <ninhhongviet2@gmail.com>
2025-08-21 22:15:53 +0700

    update toi uu code

--- Bai18_4Sum/Program.cs
+++ Bai18_4Sum/Program.cs
@@ -10,53 +10,59 @@ class Program
             Console.WriteLine(string.Join(", ", list));
         }
         Console.WriteLine("Hello, World!");
-    }
-    static IList<IList<int>> FourSum(int[] nums, int target)
-    {
-        IList<IList<int>> result = new List<IList<int>>();
-        for (int i =0; i< nums.Length - 3; ++i)
+    } 
+        public static IList<IList<int>> FourSum(int[] nums, int target)
         {
-            if( i > 0 && nums[i] == nums[i - 1]) continue;
-            if ((long)nums[i] + nums[i + 1] + nums[i + 2] + nums[i + 3] > (long)target)
+            Array.Sort(nums);
+            IList<IList<int>> result = new List<IList<int>>();
+            for (int i = 0; i < nums.Length - 3; ++i)
             {
-                break;
-            }
-                for (int j =i+1; j< nums.Length - 2;j++)
-            {
-                if( j > i + 1 && nums[j] == nums[j - 1]) continue;
-                int left = j + 1;
-                int right = nums.Length - 1;
-               
-                if ((long)nums[i] + nums[j] + nums[j + 1] + nums[j + 2] > (long)target)
+                if (i > 0 && nums[i] == nums[i - 1]) continue;
+                if ((long)nums[i] + nums[i + 1] + nums[i + 2] + nums[i + 3] > (long)target)
                 {
                     break;
                 }
-                while ( right > left)
+                if ((long)nums[i] + nums[nums.Length - 1] + nums[nums.Length - 2] +
+                nums[nums.Length - 3] < target)
+                    continue;
+
+
+                for (int j = i + 1; j < nums.Length - 2; j++)
                 {
-                    long sum = nums[i] + nums[j] + nums[left] + nums[right];
-                    if( sum == target)
-                    {
-                        result.Add( new List<int> { nums[i], nums[j], nums[left], nums[right] });
-                        left++;
-                        right--;
-                        while (right > left && nums[left] == nums[left - 1])   left++;
-                        while (right > left && nums[right] == nums[right + 1])   right--;
-                        
-                    }
+                    if (j > i + 1 && nums[j] == nums[j - 1]) continue;
+                    if ((long)nums[i] + nums[j] + nums[j + 1] + nums[j + 2] > (long)target)
+                        break;
+                    if ((long)nums[i] + nums[j] + nums[nums.Length - 1] +
+                    nums[nums.Length - 2] < target)
+                        continue;
 
-                    else if (sum < target)
+                    int left = j + 1;
+                    int right = nums.Length - 1;
+                    while (right > left)
                     {
-                        left++;
-                    }
-                    else
-                    {
-                        right--;
-                    }
+                        long sum = (long)nums[i] + nums[j] + nums[left] + nums[right];
+                        if (sum == (long)target)
+                        {
+                            result.Add(new List<int> { nums[i], nums[j], nums[left], nums[right] });
+                            left++;
+                            right--;
+                            while (right > left && nums[left] == nums[left - 1]) left++;
+                            while (right > left && nums[right] == nums[right + 1]) right--;
 
+                        }
+
+                        else if (sum < (long)target)
+                        {
+                            left++;
+                        }
+                        else
+                        {
+                            right--;
+                        }
+
+                    }
                 }
             }
-        }
         return result;
-        
     }
 }
