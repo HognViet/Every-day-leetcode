2bc0d7af3bb2a54361c2f50d64f2d43f1276af7a
HongViet <ninhhongviet2@gmail.com>
2025-08-24 21:32:27 +0700

    code

---
+++ Bai27_RemoveElement/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace Bai27_RemoveElement;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+        int x = RemoveElement(new int[] { 2,3,2,3 }, 3);
+        Console.WriteLine(x);
+    }
+    static int RemoveElement(int[] nums, int target)
+    {
+        int left = 0;
+        int right = nums.Length - 1;
+        int duplicates = 0;
+        while (right > left)
+        {
+            if (nums[right] == target)
+            {
+                right--;
+                duplicates++;
+            }
+            else if (nums[left] != target)
+            {
+                left++;
+            }
+            else
+            {
+                nums[left] = nums[right];
+                left++;
+                right--;
+                duplicates++;
+            }
+        }
+        return nums.Length - duplicates;
+    }
+}
