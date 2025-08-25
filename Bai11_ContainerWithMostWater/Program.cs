f8b07afa301b925f4231abcac3a2c4caae398866
HongViet <ninhhongviet2@gmail.com>
2025-08-25 20:53:24 +0700

    code

---
+++ Bai11_ContainerWithMostWater/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace Bai11_ContainerWithMostWater;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+        int k = MaxArea(new int[] { 1, 8, 6, 2, 5, 4, 8, 3, 7 });
+        Console.WriteLine(k);
+
+    }
+    public static int MaxArea(int[] height)
+    {
+        int max = 0;
+
+        int right = height.Length - 1;
+        int left = 0;
+
+        while (right > left)
+        {
+
+            int temp = (right - left) * Math.Min(height[right], height[left]);
+            if (max < temp) max = temp;
+
+            if (height[right] <= height[left]) right--;
+            else left++;
+        }
+        return max;
+    }
+}
