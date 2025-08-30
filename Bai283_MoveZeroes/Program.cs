d293a4c44809fa67963948395cff46707ebebeff
HongViet <ninhhongviet2@gmail.com>
2025-08-30 23:30:03 +0700

    code

---
+++ Bai283_MoveZeroes/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace Bai283_MoveZeroes;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+
+        int[] nums = args.Select(int.Parse).ToArray();
+        MoveZeroes(nums);
+        Console.WriteLine(String.Join(", ", nums));
+    }
+    public static void MoveZeroes(int[] nums)
+    {
+        int a = 0;
+        for (int i = 0; i < nums.Length; ++i)
+        {
+            if (nums[i] != 0)
+            {
+                nums[a] = nums[i];
+                a++;
+            }
+        }
+
+        for (int i = a; i < nums.Length; ++i)
+        {
+            nums[i] = 0;
+        }
+    }
+}
